158be9ee7c7398124d1c961e78961a5bc02b202b
albertokea <atorrescota5@gmail.com>
2021-01-31 22:55:34 +0100

    Lista de todos añadida a todo_datos.js

---
+++ js/todo_datos.js
@@ -0,0 +1,32 @@
+var listaTareas = [
+    {
+        idTarea: 0,
+        titulo: 'Estudiar javascript',
+        prioridad: 'urgente',
+    },
+    {
+        idTarea: 1,
+        titulo: 'Dormir',
+        prioridad: 'diaria',
+    },
+    {
+        idTarea: 2,
+        titulo: 'Salir a comer',
+        prioridad: 'mensual',
+    },
+    {
+        idTarea: 3,
+        titulo: 'Pagar el alquiler',
+        prioridad: 'mensual',
+    },
+    {
+        idTarea: 4,
+        titulo: 'Salir a correr',
+        prioridad: 'diaria',
+    },
+    {
+        idTarea: 5,
+        titulo: 'Suscribirme a 10 minutos programando',
+        prioridad: 'urgente',
+    },
+]
